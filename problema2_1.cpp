6a7627032cf4f1ce9da7cb499567b3c9c6cfd5bd
Jesus Alejandro Cordoba Pulido <jalejandrodj@gmail.com>
2017-11-10 10:46:25 +0100

    Commit rama jesus

---
+++ problema2_1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+	int main () {
+		double valor1, valor2;
+		cout <<"Introduzca el valor numero uno ";
+		cin >> valor1;
+		cout <<"Introduzca el valor numero dos " ;
+		cin >> valor2;
+
+		if (valor1 > valor2) {
+		cout <<"El valor uno es mayor que valor dos "<< endl;		
+		}
+		else {
+		cout <<"El valor dos es mayor que valor uno "<< endl;
+		}
+		
+
+}
